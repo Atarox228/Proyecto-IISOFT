07d361b7c13f23ff4a5a774d9d7ed344fd7a9ab1
LautaroVillagraM <lautarovillagram@gmail.com>
2025-04-09 21:59:53 -0300

    iconos, cambios en la card y el la grid

--- TableroLibre/src/Components/ProductCard/ProductCard.jsx
+++ TableroLibre/src/Components/ProductCard/ProductCard.jsx
@@ -1,6 +1,10 @@
 import './ProductCard.css'
-import category from '../../assets/category.png'
-import uno from '../../assets/uno.png'
+import cards from '../../assets/cards.png'
+import location from '../../assets/location.png'
+import age from '../../assets/age.png'
+import duration from '../../assets/duration.png'
+import players from '../../assets/players.png'
+
 
 const ProductCard = ({product}) => {
     return (<>
@@ -8,25 +12,29 @@ const ProductCard = ({product}) => {
                 <div className='card'>
                 <div className='card-header'>
                     <p>{product.name}</p>    
-                    <img src={category} className='category-icon'/> 
+                    <img src={cards} className='category-icon'/> 
                 </div>               
                 <img src={product.image_url} alt="product image" className='card-image'/>
 
                 <div className='card-characteristics'></div>
                <div className='card-detail'>
-               <p>{product.localization}</p>
+                <img src={location} className='detail-icon'/>
+                <p>{product.localization}</p>
                </div>
                <div className='card-detail'>
+                <img src={age} className='detail-icon'/>
                <p>{product.age}</p>
                </div>
                <div className='card-detail'>
+                <img src={duration} className='detail-icon'/>
                <p>{product.duration}</p>
                </div>
                <div className='card-detail'>
+                <img src={players} className='detail-icon'/>
                <p>{product.players}</p>
                </div>
                <div className='card-price'>
-                <h1>{product.price}</h1>
+                <h1>$ {product.price}</h1>
                </div>
                
                
