7d7e5709979bdae4294365b66a68d28d00cc3e47
LautaroVillagraM <lautarovillagram@gmail.com>
2025-04-11 15:54:48 -0300

    agregadas las categorias y los iconos, cambios en el diseño de la ProductCard

--- TableroLibre/src/Components/ProductCard/ProductCard.jsx
+++ TableroLibre/src/Components/ProductCard/ProductCard.jsx
@@ -1,5 +1,7 @@
 import './ProductCard.css'
-import cards from '../../assets/cards.png'
+import cards from '../../assets/cards-icon.png'
+import dices from '../../assets/dices-icon.png'
+import board from '../../assets/board-icon.png'
 import location from '../../assets/location.png'
 import age from '../../assets/age.png'
 import duration from '../../assets/duration.png'
@@ -7,16 +9,23 @@ import players from '../../assets/players.png'
 
 
 const ProductCard = ({product, onClick}) => {
+
+    const categoryIcons = {
+        cartas: cards,
+        dados: dices,
+        tablero: board,
+      }
+
     return (<>
             <div className='productcard-wrapper' onClick={onClick}> 
                 <div className='card'>
                 <div className='card-header'>
                     <p>{product.name}</p>    
-                    <img src={cards} className='category-icon'/> 
+                    <img src={categoryIcons[product.category]} className='category-icon'/> 
                 </div>               
                 <img src={product.image_url} alt="product image" className='card-image'/>
 
-                <div className='card-characteristics'></div>
+                <div className='card-characteristics'>
                <div className='card-detail'>
                 <img src={location} className='detail-icon'/>
                 <p>{product.localization}</p>
@@ -32,6 +41,7 @@ const ProductCard = ({product, onClick}) => {
                <div className='card-detail'>
                 <img src={players} className='detail-icon'/>
                <p>{product.players}</p>
+               </div>
                </div>
                <div className='card-price'>
                 <h1>$ {product.price}</h1>
