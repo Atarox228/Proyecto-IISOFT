2ec3405abd9af7f979768c2c89412f4bfbeaa1e0
Santino <scargnelutti@protonmail.com>
2025-04-11 13:01:43 -0300

    transferido height de root, html y body a .page-wrapper

--- TableroLibre/src/Components/Product/Product.jsx
+++ TableroLibre/src/Components/Product/Product.jsx
@@ -1,55 +1,40 @@
 import './Product.css'
-import {useEffect, useState} from "react";
-import {getProductById} from "../../db/queries.jsx";
 
 const Product = ({ product }) => {
 
-
-/*  
-  useEffect(() => {
-    getProductById(id)
-        .then((data) => {
-          setProduct(data);
-        });
-  }, []);
-
-*/
-
-
   return (
-      <>
-        <div className="container">
-          <div className="resumenProducto">
-            <div className="contenedorImagen">
-              <img src={product.image_url} className="imagen"/>
-            </div>
-            <div className="informacionBasica">
-              <p>{product.name}</p>
-              <p className="nombreVendedor">De Juan Pérez</p>
-              <p><strong>$ {product.price}</strong></p>
-            </div>
+      <div className="container">
+        <div className="resumenProducto">
+          <div className="contenedorImagen">
+            <img src={product.image_url} className="imagen"/>
           </div>
-          <div className="caracteristicas">
-            <p className="textoSeccion">Características</p>
-            <hr />
-            <div className="informacionDeProducto">
-              <p>Edad mínima: {product.age}</p>
-              <p>Duración estimada: {product.duration}</p>
-              <p>Cantidad de jugadores: {product.players}</p>
-            </div>
+          <div className="informacionBasica">
+            <p>{product.name}</p>
+            <p className="nombreVendedor">De Juan Pérez</p>
+            <p><strong>$ {product.price}</strong></p>
           </div>
-          <div className="manual">
-            <p className="textoSeccion">Manual de usuario</p>
-            <hr />
-            <embed
-                src={product.user_manual}
-                width="100%"
-                height="85%"/>
-          </div>
-
         </div>
-      </>
-  )
+        <div className="caracteristicas">
+          <p className="textoSeccion">Características</p>
+          <hr />
+          <div className="informacionDeProducto">
+            <p>Edad mínima: {product.age}</p>
+            <p>Duración estimada: {product.duration}</p>
+            <p>Cantidad de jugadores: {product.players}</p>
+          </div>
+        </div>
+        <div className="manual">
+          <p className="textoSeccion">Manual de usuario</p>
+          <hr />
+          <embed
+              src={product.user_manual}
+              width="100%"
+              height="85%"/>
+        </div>
+
+      </div>
+
+  );
 }
 
 export default Product;
