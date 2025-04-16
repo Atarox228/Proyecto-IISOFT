96725d15afc49151cd6222218860717d0de1e55a
Atarox <80927839+Atarox228@users.noreply.github.com>
2025-04-16 00:37:10 -0300

    Scrollbar agregada, modificacion tamaño de cartas

--- TableroLibre/src/Components/ProductCard/ProductCard.jsx
+++ TableroLibre/src/Components/ProductCard/ProductCard.jsx
@@ -19,33 +19,32 @@ const ProductCard = ({product, onClick}) => {
     return (<>
             <div className='productcard-wrapper' onClick={onClick}> 
                 <div className='card'>
-                <div className='card-header'>
-                    <h3>{product.name}</h3>    
-                    <img src={categoryIcons[product.category]} className='category-icon'/> 
-                </div>               
-                <img src={product.image_url} alt="product image" className='card-image'/>
-
-                <div className='card-characteristics'>
-               <div className='card-detail'>
-                <img src={location} className='detail-icon'/>
-                <p>{product.location}</p>
-               </div>
-               <div className='card-detail'>
-                <img src={age} className='detail-icon'/>
-               <p>{product.age}+</p>
-               </div>
-               <div className='card-detail'>
-                <img src={duration} className='detail-icon'/>
-               <p>{product.duration}'</p>
-               </div>
-               <div className='card-detail'>
-                <img src={players} className='detail-icon'/>
-               <p>{product.players}</p>
-               </div>
-               </div>
-               <div className='card-price'>
-                <h1>$ {product.price}</h1>
-               </div>
+                    <div className='card-header'>
+                        <h3>{product.name}</h3>    
+                        <img src={categoryIcons[product.category]} className='category-icon'/> 
+                    </div>               
+                    <img src={product.image_url} alt="product image" className='card-image'/>
+                    <div className='card-characteristics'>
+                <div className='card-detail'>
+                    <img src={location} className='detail-icon'/>
+                    <p>{product.location}</p>
+                </div>
+                <div className='card-detail'>
+                    <img src={age} className='detail-icon'/>
+                <p>{product.age}+</p>
+                </div>
+                <div className='card-detail'>
+                    <img src={duration} className='detail-icon'/>
+                <p>{product.duration}'</p>
+                </div>
+                <div className='card-detail'>
+                    <img src={players} className='detail-icon'/>
+                <p>{product.players}</p>
+                </div>
+                </div>
+                <div className='card-price'>
+                    <h1>$ {product.price}</h1>
+                </div>
                
                
                
