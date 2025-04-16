77d6b62ce805c5640fa148144d69190e34c03404
Santino <scargnelutti@protonmail.com>
2025-04-16 12:16:54 -0300

    borrado codigo comentado y modificada asignacion de key en productgrid

--- TableroLibre/src/Components/Product/Product.jsx
+++ TableroLibre/src/Components/Product/Product.jsx
@@ -4,10 +4,6 @@ import {useEffect, useState} from "react";
 import {getProductById} from "../../db/queries.jsx";
 import manual from '../../assets/manual.png';
 import returnIcon from '../../assets/return.png';
-import location from '../../assets/location.png'
-import age from '../../assets/age.png'
-import duration from '../../assets/duration.png'
-import players from '../../assets/players.png'
 
 
 const Product = () => {
@@ -24,7 +20,7 @@ const Product = () => {
   }, []);
 
   if (!product) {
-    return (<div>Cargando producto...</div>);
+    return (<div>Cargando...</div>);
   }
 
   return (
@@ -39,46 +35,34 @@ const Product = () => {
             </div>
         ) : (
             <>
-
               <div className="resumenProducto">
                 <div className='imagenYResumen'>
-                <div className="contenedorImagen">
-                  <img src={product.image_url} className="imagen" alt={"Imagen del juego"}/>
-                </div>
-                <div className="informacionBasica">
-                  <h1>{product.name}</h1>
-                  <p className="nombreVendedor">De Juan Pérez</p>
-                  <p>Categoría: {product.category}</p>
-                  <p>Lugar: {product.location}</p>
-                  <p>Edad mínima: {product.age}</p>
-                  <p>Duración estimada: {product.duration} min.</p>
-                  <p>Cantidad de jugadores: {product.players}</p>
-                  <p><strong>$ {product.price}</strong></p>
-                  <button className="botonManual" onClick={() => setEsManualVisible(true)} type="button">
-                    <img className='logoManual' src={manual}/>
-                    <p>Ver instrucciones de juego</p>
-                  </button>
+                  <div className="contenedorImagen">
+                    <img src={product.image_url} className="imagen" alt={"Imagen del juego"}/>
+                  </div>
+                  <div className="informacionBasica">
+                    <h1>{product.name}</h1>
+                    <p className="nombreVendedor">De Juan Pérez</p>
+                    <p>Categoría: {product.category}</p>
+                    <p>Lugar: {product.location}</p>
+                    <p>Edad mínima: {product.age}</p>
+                    <p>Duración estimada: {product.duration} min.</p>
+                    <p>Cantidad de jugadores: {product.players}</p>
+                    <p><strong>$ {product.price}</strong></p>
+                    <button className="botonManual" onClick={() => setEsManualVisible(true)} type="button">
+                      <img className='logoManual' src={manual}/>
+                      <p>Ver instrucciones de juego</p>
+                    </button>
+                  </div>
                 </div>
               </div>
+              <div className="seccion">
+                <p className="textoSeccion">Descripción</p>
+                <hr />
+                <p>{product.description}</p>
               </div>
-            <div className="seccion">
-            <p className="textoSeccion">Descripción</p>
-            <p>{product.description}</p>
-            <hr />
-            <div className="informacionDeProducto">
-
-            </div>
-            </div>
-            {/*<div className="seccion">*/}
-            {/*  <p className="textoSeccion">Descripción</p>*/}
-            {/*  <hr />*/}
-            {/*  <p>{product.descripcion}*/}
-            {/*  </p>*/}
-            {/*</div>*/}
             </>
         )}
-
-
       </div>
   );
 }
