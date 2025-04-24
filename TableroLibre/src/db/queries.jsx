236f4c93b9304e157791d34c6069973a46c5a932
LautaroVillagraM <lautarovillagram@gmail.com>
2025-04-24 01:33:51 -0300

    agregadas querys

--- TableroLibre/src/db/queries.jsx
+++ TableroLibre/src/db/queries.jsx
@@ -41,6 +41,41 @@ export const getIdOfGameByName = async (name) => {
   return data[0].id;
 }
 
+export const reserveProduct = async ({ idProducto, idUsuarioReserva }) => {
+  if (!idProducto || !idUsuarioReserva) {
+    return { success: false, message: "Faltan parámetros para reservar el producto" };
+  }
+
+  const product = await getProductById(idProducto); 
+
+  if (!product) {
+    return { success: false, message: "Producto no encontrado" };
+  }
+
+  if (product.id_buyer === null) {
+    const { data, error } = await supabase
+      .from("Productos")
+      .update({ id_buyer: idUsuarioReserva })
+      .eq('id', idProducto);
+
+    if (error) {
+      return { success: false, message: "Error al reservar el producto" };
+    } else {
+      return { success: true, message: "" };
+    }
+  } else {
+    return { success: false, message: "Error al confirmar la reserva: el producto ya fue reservado" };
+  }
+};
+
+export const getSellerInformation = async ({userVendedor}) => {
+  const {data, error} = await supabase.from('perfiles').select('*').eq('username', userVendedor);
+  if (error) {
+    console.log(error);
+  } 
+  return data[0];
+}
+
 export const createProduct = async ({ idDeJuego, ubicacion, precio, descripcion, seller_username }) => {
   const { data, errorInsert } = await supabase
     .from("Productos")
@@ -59,10 +94,11 @@ export const createProduct = async ({ idDeJuego, ubicacion, precio, descripcion,
 };
 
 export const cancelReserve = async({id_product}) => {
+  
   const {data, error} = await supabase.from("Productos").update({id_buyer: null}).eq('id', id_product);
   if (error) {
     console.log(error);
   } else {
-    console.log(cancelado);
+    console.log('cancelado');
   }
 }
