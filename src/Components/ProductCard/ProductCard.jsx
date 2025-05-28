aaaca6e8ee8b056fa3856a39fc77966d4d4b6a86
LautaroVillagraM <lautarovillagram@gmail.com>
2025-05-28 00:23:39 -0300

    ajuste timing cartas

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -24,7 +24,7 @@ const ProductCard = ({ product, onClick }) => {
     setVisible(true);
     const timer = setTimeout(() => {
       setGirada(true);
-    }, 1000);
+    }, 500);
     return () => clearTimeout(timer);
   }, []);
 
